8c5cbbd3056409081159eab8a088e84a68cd7fdd
bena-sudo <joanbenaventcastells@gmail.com>
2024-10-02 17:36:00 +0200

    Función que recibe un producto y devuelve un elemento div con el detalle.

---
+++ consum/detalls_producte.js
@@ -0,0 +1,35 @@
+function renderProducte(producte) {
+  let divProducte = document.createElement("div");
+  let textoDiv = `
+  <h1 class="text-center h1">${producte.nom}</h1>
+  <img src="${producte.foto} class="img-fluid" alt="${producte.nom}" />
+  <div id="divGaleria">Galeria<div>
+  <p class="lead">${producte.descripcio}</p>
+  <table class="table table-striped table-hover">
+    <tr>
+      <td>Preu: </td>
+      <td>${producte.preu}</td>
+    </tr>
+    <tr>
+      <td>Marca: </td>
+      <td>${producte.marca}</td>
+    </tr>
+    <tr>
+      <td>Categoria: </td>
+      <td>${producte.categoria}</td>
+    </tr>
+    <tr>
+      <td>Pes: </td>
+      <td>${producte.pes}</td>
+    </tr>
+    <tr>
+      <td>Dimensions: </td>
+      <td>${producte.dimensions}</td>
+    </tr>
+  </table>
+  <div id="divComentari">Comentaris<div>
+   </div>`;
+
+  divProducte.innerHTML = textoDiv;
+  return divProducte;
+}
