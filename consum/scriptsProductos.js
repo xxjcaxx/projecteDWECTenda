1bb52dd032a38ee086accf551bf073aceb118a30
Pau <palorent.7@gmail.com>
2024-10-02 18:20:05 +0200

    envio script productos

---
+++ consum/scriptsProductos.js
@@ -0,0 +1,32 @@
+export {renderProductos};
+// PRODUCTES : id, nom, preu, descripcio, marca, categoria, pes, dimensions, foto
+function renderProductos(productes){
+    let divProductos = productes
+        .map((p) => `
+  <div class="col">
+  <div class="card" style="width: 18rem;">
+  <img src="/img-placeholder.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${p.nom}</h5>
+    <p class="card-text">${p.descripcio}</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">${p.categoria}</li>
+    <li class="list-group-item">${p.marca}</li>
+    <li class="list-group-item">Marca: ${p.pes}</li>
+    <li class="list-group-item">Precio: ${p.dimensions}</li>
+    <li class="list-group-item">${p.preu}</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link btn btn-success" >Añadir al carrito</a>
+  </div>
+  </div>
+  </div>
+  `
+
+        )
+        .join('');
+        return divProductos;
+  }
+  
+  
