ec9bc933ffe808cea7ee4ab291b4d6a20140e409
bena-sudo <joanbenaventcastells@gmail.com>
2024-10-23 18:20:57 +0200

    v1

--- consum/main.js
+++ consum/main.js
@@ -3,3 +3,7 @@ import './style/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+import { getComentaris } from './view/comentaris_supabase'
+
+document.addEventListener("DOMContentLoaded" ,async() => { await console.log(JSON.stringify(getComentaris(1)))
+})
