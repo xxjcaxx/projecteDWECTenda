ef2f79e529970d8f32d0640cbcaef8a553d7f3b0
kin85 <quino.l85@gmail.com>
2024-12-04 18:57:14 +0100

    33v2-pagina-productes

--- consum/WebComponents/ProductosComponent.js
+++ consum/WebComponents/ProductosComponent.js
@@ -33,9 +33,7 @@ export class ProductosComponent extends HTMLElement {
   </div>
   </div>
   </div>
-  `
-      )
-      .join("");
+  `).join("");
 
     this.innerHTML = `
           <div>${divProductos}</div>
